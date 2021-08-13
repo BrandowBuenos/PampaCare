1a9081138491310864a5082e7dd96994914a7aa1
Brandow Buenos <brandowbuenos@gmail.com>
2021-08-13 13:07:50 -0300

    feat: create import breed
    Co-authored-by: Willian Giovanni Pires Clemente <willianpires75@gmail.com>

--- src/controllers/breed/ImportBreed.ts
+++ src/controllers/breed/ImportBreed.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from 'express'
+import csv from 'csv-parser'
+import fs from 'fs'
 
+const result: any = []
 class ImportBreed {
   async execute (request: Request, response: Response): Promise<Response> {
     try {
-      return response.status(200).json('message')
+      fs.createReadStream('/Volumes/SSD/dev/PampaCare/src/csv/example.csv')
+        .pipe(csv())
+        .on('data', (data) => result.push(data))
+        .on('end', () => {
+          console.log(result)
+        })
+
+      var jsonArray = JSON.parse(JSON.stringify(result))
+
+      return response.status(200).json(jsonArray.map((item: { Raça: String }) => item.Raça))
     } catch (error) {
       return response.status(400).json({
         message: error.message
