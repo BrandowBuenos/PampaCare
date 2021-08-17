98ead0ebe02b6471a625cfcca595678d24a8e948
Brandow Buenos <brandowbuenos@gmail.com>
2021-08-17 17:13:58 -0300

    feat: implement multiples read in ImportBreed
    Co-authored-by: Willian Giovanni Pires Clemente <willianpires75@gmail.com>

--- src/controllers/breed/ImportBreed.ts
+++ src/controllers/breed/ImportBreed.ts
@@ -2,30 +2,42 @@ import { Request, Response } from 'express'
 import csv from 'csv-parser'
 import fs from 'fs'
 import prisma from '../../services/prisma'
-
+import { listFilesFromFolder } from '../../services/readAllCsvFiles'
 class ImportBreed {
   async execute (request: Request, response: Response): Promise<Response> {
     try {
       const result: any = []
+      const filesList = listFilesFromFolder('src/files/csv')
 
-      fs.createReadStream('src/csv/example.csv')
-        .pipe(csv())
-        .on('data', async (data) => {
-          await result.push(data)
-        })
-        .on('end', async () => {
-          const jsonArray = JSON.parse(JSON.stringify(result))
-          const resultado = jsonArray.map((item: { Raça: String }) => item.Raça)
+      for (let i = 0; i < filesList.length; i++) {
+        fs.createReadStream(filesList[i])
+          .pipe(csv())
+          .on('data', async (data) => {
+            await result.push(data)
+          })
+          .on('end', async () => {
+            const jsonArray = JSON.parse(JSON.stringify(result))
 
-          resultado.forEach(async (breedName: string) => {
-            await prisma.raca.createMany({
-              skipDuplicates: true,
-              data: {
-                nome: breedName
-              }
+            // const resultadoFinal = filterRelatedWords(jsonArray, ['raça', 'raças', 'Raca', 'Raça'])
+
+            const array1 = jsonArray.map((item: { Raça: String }) => item.Raça)
+            const array2 = jsonArray.map((item: { raça: String }) => item.raça)
+            const array3 = jsonArray.map((item: { raca: String }) => item.raca)
+
+            const resultado = array1.concat(array2).concat(array3)
+
+            const resultadoFinal = resultado.filter((item: string) => item !== undefined)
+
+            resultadoFinal.forEach(async (breedName: string) => {
+              await prisma.raca.createMany({
+                skipDuplicates: true,
+                data: {
+                  nome: breedName.toLowerCase()
+                }
+              })
             })
           })
-        })
+      }
 
       return response.status(201).json({ message: 'Importação de raças efetuada com sucesso !' })
     } catch (error) {
